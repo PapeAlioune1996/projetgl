a7cc3534b28762da4f2c30ee7844660159291fb3
Papealioune96 <germainalioune96@gmail.com>
2020-11-25 16:22:55 +0100

    update test

--- src/main/java/calculatrices/Test.java
+++ src/main/java/calculatrices/Test.java
@@ -3,14 +3,23 @@ package calculatrices;
 public class Test {
 	public static void main(String[] args)
 	 {
+		int[] t= {2,7,9,3};
 		 Calculator c=new Calculator(); 
 		int s= c.sum(4,5);
 		int d= c.munis(4,5);
-		int e= c.min(4,5);
-		int m= c.max(4,5);
-		System.out.println(s);
-		System.out.println(d);
-		System.out.println(e);
-		System.out.println(m);
+		int p= c.multiply(4,5);
+		int div= c.divide(9,3);
+		int min= c.munis(4,5);
+		int max= c.munis(4,5);
+		int e= c. minElement(t);
+		int m= c. maxElement(t);
+		System.out.println("La somme est:"+s);
+		System.out.println("le reste est:"+d);
+		System.out.println("Le produit est:"+p);
+		System.out.println("Le quotient est:"+div);
+		System.out.println("Le minmum est:"+min);
+		System.out.println("Le maximum est:"+max);
+		System.out.println("Le minmum du tableau est:"+e);
+		System.out.println("Le maximum du tableau est:"+m);
 	 }
 }
